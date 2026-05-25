3e637f7facbe9eef603f33666f87eaaedc88098f
involvex <39644169+involvex@users.noreply.github.com>
2026-05-25 10:55:48 +0200

    chore: add .github/FUNDING.yml

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
-const GRADLE_GITHUB_RELEASE = 'https://github.com/gradle/gradle/releases/latest'
-const GRADLE_DISTIBUTIONS_BASE_URL =
-	'https://services.gradle.org/distributions/'
+export const GRADLE_GITHUB_RELEASE =
+  "https://github.com/gradle/gradle/releases/latest";
+export const GRADLE_DISTIBUTIONS_BASE_URL =
+  "https://services.gradle.org/distributions/";
 // url should be = https://services.gradle.org/distributions/gradle-9.5.1-bin.zip
 //https://services.gradle.org/distributions/gradle-<version>-bin.zip
