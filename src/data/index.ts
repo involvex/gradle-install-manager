fa785204c002589e6e77d72ecd7b94c731b71289
involvex <39644169+involvex@users.noreply.github.com>
2026-05-25 03:44:46 +0200

    init

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+const GRADLE_GITHUB_RELEASE = 'https://github.com/gradle/gradle/releases/latest'
+const GRADLE_DISTIBUTIONS_BASE_URL =
+	'https://services.gradle.org/distributions/'
+// url should be = https://services.gradle.org/distributions/gradle-9.5.1-bin.zip
+//https://services.gradle.org/distributions/gradle-<version>-bin.zip
